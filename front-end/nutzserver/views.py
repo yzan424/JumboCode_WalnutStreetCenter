b2a3eb4ddc82a20a8e1c01d99d2f24da56f3a579
holeyness <ian@ianluo.com>
2016-03-02 19:48:45 -0500

    Created sample profile using django

--- front-end/nutzserver/views.py
+++ front-end/nutzserver/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from nutzserver.models import *
-import json
+import json, requests
 from django.core import serializers
 
+backendGET = 'http://localhost:8000/backend/profile/'
+
 # Create your views here.
-def backend(request):
+def backend(request, profile_id):
     if request.method == 'GET':
-        profile = int(request.GET.get('profile'))
+        profile = int(profile_id)
         if profile > 0:
             emfprofile = EMFProfile.objects.filter(id=profile)
         else:
@@ -17,3 +19,16 @@ def backend(request):
         return JsonResponse(emfprofile, safe=False)
     elif request.method == 'POST':    
         pass
+
+def profile(request):
+    profile_data = requests.get(backendGET + '0') #make a request to the backend
+    profile_data = json.loads(profile_data.json()) # parse response 
+    profile_fields = profile_data[0]['fields']
+
+    print(profile_fields)
+
+    for x in profile_data[0]['fields']:
+        print(x)
+        print(profile_data[0]['fields'][x])
+
+    return render(request, "profile.html", context={'profile_fields': profile_fields})
