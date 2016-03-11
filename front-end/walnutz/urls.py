0b947890b428e7e50bc86879e8d30d3309e429c5
Jordan Kemp <jtk296@gmail.com>
2016-03-11 00:20:07 -0500

    Added ability to edit data

--- front-end/walnutz/urls.py
+++ front-end/walnutz/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^backend/profile/(?P<profile_id>\d+)/$', backend),
     url(r'^profile/(?P<profile_id>\d+)/$', profile),
-    url(r'^update/(?P<profile_id>\d+)/$', update),
+    url(r'^update/(?P<profile_id>\d+)/', update),
 
 ]
