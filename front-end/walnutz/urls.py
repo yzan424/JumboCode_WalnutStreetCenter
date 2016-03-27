fe7fde5772a2d9dbaf40a268a0fe1c13102ca383
holeyness <ian@ianluo.com>
2016-03-27 14:24:53 -0400

    Fixed fake backend

--- front-end/walnutz/urls.py
+++ front-end/walnutz/urls.py
@@ -19,7 +19,7 @@ from nutzserver.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^backend/profile/(?P<profile_id>\d+)/$', backend),
+    url(r'^backend/profile/(?P<profile_id>\d+)$', backend),
     url(r'^profile/(?P<profile_id>\d+)/$', profile),
     url(r'^update/(?P<profile_id>\d+)/', update),
     url(r'^signin/$', login),
