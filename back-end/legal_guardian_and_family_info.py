f987145ef485df6fe57cb4288f97039229eef068
Meet Patel <mjtpatel@gmail.com>
2016-03-30 22:13:33 -0400

    changed table syntax

---
+++ back-end/legal_guardian_and_family_info.py
@@ -0,0 +1,19 @@
+class legal_guardian_and_family_info(db.model):
+    __tablename__ = 'legal_guardian_and_family_info'
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id), unique=True, nullable=False)
+    guardian_name = db.Column(db.String)
+    guardian_phone = db.Column(db.String)
+    guardian_address = db.Column(db.String)
+    father_name = db.Column(db.String)
+    father_birthday = db.Column(db.Date)
+    father_birthplace = db.Column(db.String)
+    father_alive = db.Column(db.Boolean)
+    mother_maiden_name = db.Column(db.String)
+    mother_birthday = db.Column(db.Date)
+    mother_birthplace = db.Column(db.String)
+    mother_alive = db.Column(db.Boolean)
+    parents_martial_status = db.Column(db.String)
+    family_phone = db.Column(db.String(10))
+    family_address = db.Column(db.String)
+
