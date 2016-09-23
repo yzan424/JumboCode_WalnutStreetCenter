bbe68a80c1c6e28ee200417be7dd42893af74faf
holeyness <ian@ianluo.com>
2016-09-23 11:18:05 -0400

    Change code for deploy

--- walnut_street/__init__.py
+++ walnut_street/__init__.py
@@ -6,6 +6,6 @@ from flask import Flask, url_for
 application_directory = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://localhost/walnut_dev'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql:///walnut_dev'
 
 app.debug = True
